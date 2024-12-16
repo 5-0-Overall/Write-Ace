337b3d578464ab77e9b628c9b52c31a82c5c81cd
lshigami <22120334@student.hcmus.edu.vn>
2024-12-16 21:14:13 +0700

    Enhance API controllers and services with authentication and new endpoints
    - Added authentication guards to OpenAI, Problem, Submission, and Tag controllers.
    - Introduced new endpoint in ProblemController to fetch a problem by ID.
    - Implemented a new method in SubmissionService to retrieve submissions by user ID.
    - Updated SubmissionController with additional operations for managing submissions.
    - Enhanced Swagger documentation with APIBearerAuth and operation summaries for better clarity.

--- src/BE/writeace/src/modules/submission/submission.controller.ts
+++ src/BE/writeace/src/modules/submission/submission.controller.ts
@@ -1,36 +1,43 @@
-import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Post, Put, UseInterceptors } from "@nestjs/common";
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Post, Put, Req, UseGuards, UseInterceptors } from "@nestjs/common";
 import { SubmissionService } from "./submission.service";
 import { SubmissionEntity } from "./entity/submission.entity";
 import { SubmissionCreateDTO } from "./dto/submission.dto.request";
-import { ApiTags, ApiOperation, ApiResponse, ApiBody, ApiParam } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody, ApiParam, ApiBearerAuth } from '@nestjs/swagger';
 import { ContributionService } from "../contribution/contribution.service";
 import { OpenAIUpdateSubmissionDTO } from "./dto/openai.update.submission.dto";
+import { AuthGuard } from "../guard/auth.guard";
 
 
 
 @ApiTags('submission')
 @Controller('submissions')
+@ApiBearerAuth()
+@UseGuards(AuthGuard)
 @UseInterceptors(ClassSerializerInterceptor)  
 export class SubmissionController {
   constructor(private submissionService: SubmissionService,private contributionService: ContributionService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all submissions' })
   async getAllSubmissions(): Promise<SubmissionEntity[]> {
     return this.submissionService.getAllSubmissions();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get submission by id' })
   async getSubmissionById(@Param('id') id: number): Promise<SubmissionEntity> {
     return this.submissionService.getSubmissionById(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new submission' })
   async createSubmission(@Body() submission: SubmissionCreateDTO): Promise<SubmissionEntity> {
     this.contributionService.incrementContribution(submission.user);
     return this.submissionService.createSubmission(submission);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update submission' })
   async updateSubmission(
     @Body() submission: OpenAIUpdateSubmissionDTO
   ): Promise<SubmissionEntity> {
@@ -38,7 +45,14 @@ export class SubmissionController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete submission' })
   async deleteSubmission(@Param('id') id: number): Promise<void> {
     return this.submissionService.deleteSubmission(id);
+  }
+
+  @Get('me')
+  @ApiOperation({ summary: 'Get my submissions' })
+  async getMySubmission(@Req() req: RequestWithUser): Promise<SubmissionEntity[]> {
+    return this.submissionService.getSubmissionByUserId(req.user.id);
   }
 }
